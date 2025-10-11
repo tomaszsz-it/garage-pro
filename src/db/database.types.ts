0dcbd07e17076c4627057d4eed0b4219a6a89263
Tomasz Szymczuk <tomaszsz.it@gmail.com>
2025-10-11 20:16:23 +0200

    chore: add supabase connectivity
     Generowanie kontraktów i endpointów Rest API - dodanie typów

---
+++ src/db/database.types.ts
@@ -0,0 +1,552 @@
+export type Json = string | number | boolean | null | { [key: string]: Json | undefined } | Json[];
+
+export interface Database {
+  graphql_public: {
+    Tables: Record<never, never>;
+    Views: Record<never, never>;
+    Functions: {
+      graphql: {
+        Args: {
+          extensions?: Json;
+          operationName?: string;
+          query?: string;
+          variables?: Json;
+        };
+        Returns: Json;
+      };
+    };
+    Enums: Record<never, never>;
+    CompositeTypes: Record<never, never>;
+  };
+  public: {
+    Tables: {
+      employee_schedules: {
+        Row: {
+          employee_id: string;
+          end_ts: string;
+          id: string;
+          start_ts: string;
+        };
+        Insert: {
+          employee_id: string;
+          end_ts: string;
+          id?: string;
+          start_ts: string;
+        };
+        Update: {
+          employee_id?: string;
+          end_ts?: string;
+          id?: string;
+          start_ts?: string;
+        };
+        Relationships: [
+          {
+            foreignKeyName: "employee_schedules_employee_id_fkey";
+            columns: ["employee_id"];
+            isOneToOne: false;
+            referencedRelation: "employees";
+            referencedColumns: ["id"];
+          },
+        ];
+      };
+      employees: {
+        Row: {
+          created_at: string;
+          email: string;
+          id: string;
+          name: string;
+          surname: string;
+          type: string;
+        };
+        Insert: {
+          created_at?: string;
+          email: string;
+          id?: string;
+          name: string;
+          surname: string;
+          type: string;
+        };
+        Update: {
+          created_at?: string;
+          email?: string;
+          id?: string;
+          name?: string;
+          surname?: string;
+          type?: string;
+        };
+        Relationships: [];
+      };
+      reservations: {
+        Row: {
+          created_at: string;
+          created_by: string;
+          employee_id: string;
+          end_ts: string;
+          id: string;
+          recommendation_text: string;
+          service_id: number;
+          start_ts: string;
+          status: string;
+          updated_at: string;
+          user_id: string;
+          vehicle_license_plate: string;
+        };
+        Insert: {
+          created_at?: string;
+          created_by: string;
+          employee_id: string;
+          end_ts: string;
+          id?: string;
+          recommendation_text?: string;
+          service_id: number;
+          start_ts: string;
+          status: string;
+          updated_at?: string;
+          user_id: string;
+          vehicle_license_plate: string;
+        };
+        Update: {
+          created_at?: string;
+          created_by?: string;
+          employee_id?: string;
+          end_ts?: string;
+          id?: string;
+          recommendation_text?: string;
+          service_id?: number;
+          start_ts?: string;
+          status?: string;
+          updated_at?: string;
+          user_id?: string;
+          vehicle_license_plate?: string;
+        };
+        Relationships: [
+          {
+            foreignKeyName: "reservations_employee_id_fkey";
+            columns: ["employee_id"];
+            isOneToOne: false;
+            referencedRelation: "employees";
+            referencedColumns: ["id"];
+          },
+          {
+            foreignKeyName: "reservations_service_id_fkey";
+            columns: ["service_id"];
+            isOneToOne: false;
+            referencedRelation: "services";
+            referencedColumns: ["service_id"];
+          },
+          {
+            foreignKeyName: "reservations_vehicle_license_plate_fkey";
+            columns: ["vehicle_license_plate"];
+            isOneToOne: false;
+            referencedRelation: "vehicles";
+            referencedColumns: ["license_plate"];
+          },
+        ];
+      };
+      services: {
+        Row: {
+          created_at: string;
+          description: string | null;
+          duration_minutes: number;
+          name: string;
+          service_id: number;
+        };
+        Insert: {
+          created_at?: string;
+          description?: string | null;
+          duration_minutes: number;
+          name: string;
+          service_id?: number;
+        };
+        Update: {
+          created_at?: string;
+          description?: string | null;
+          duration_minutes?: number;
+          name?: string;
+          service_id?: number;
+        };
+        Relationships: [];
+      };
+      vehicles: {
+        Row: {
+          brand: string | null;
+          car_type: string | null;
+          created_at: string;
+          license_plate: string;
+          model: string | null;
+          production_year: number | null;
+          user_id: string;
+          vin: string | null;
+        };
+        Insert: {
+          brand?: string | null;
+          car_type?: string | null;
+          created_at?: string;
+          license_plate: string;
+          model?: string | null;
+          production_year?: number | null;
+          user_id: string;
+          vin?: string | null;
+        };
+        Update: {
+          brand?: string | null;
+          car_type?: string | null;
+          created_at?: string;
+          license_plate?: string;
+          model?: string | null;
+          production_year?: number | null;
+          user_id?: string;
+          vin?: string | null;
+        };
+        Relationships: [];
+      };
+    };
+    Views: Record<never, never>;
+    Functions: {
+      gbt_bit_compress: {
+        Args: { "": unknown };
+        Returns: unknown;
+      };
+      gbt_bool_compress: {
+        Args: { "": unknown };
+        Returns: unknown;
+      };
+      gbt_bool_fetch: {
+        Args: { "": unknown };
+        Returns: unknown;
+      };
+      gbt_bpchar_compress: {
+        Args: { "": unknown };
+        Returns: unknown;
+      };
+      gbt_bytea_compress: {
+        Args: { "": unknown };
+        Returns: unknown;
+      };
+      gbt_cash_compress: {
+        Args: { "": unknown };
+        Returns: unknown;
+      };
+      gbt_cash_fetch: {
+        Args: { "": unknown };
+        Returns: unknown;
+      };
+      gbt_date_compress: {
+        Args: { "": unknown };
+        Returns: unknown;
+      };
+      gbt_date_fetch: {
+        Args: { "": unknown };
+        Returns: unknown;
+      };
+      gbt_decompress: {
+        Args: { "": unknown };
+        Returns: unknown;
+      };
+      gbt_enum_compress: {
+        Args: { "": unknown };
+        Returns: unknown;
+      };
+      gbt_enum_fetch: {
+        Args: { "": unknown };
+        Returns: unknown;
+      };
+      gbt_float4_compress: {
+        Args: { "": unknown };
+        Returns: unknown;
+      };
+      gbt_float4_fetch: {
+        Args: { "": unknown };
+        Returns: unknown;
+      };
+      gbt_float8_compress: {
+        Args: { "": unknown };
+        Returns: unknown;
+      };
+      gbt_float8_fetch: {
+        Args: { "": unknown };
+        Returns: unknown;
+      };
+      gbt_inet_compress: {
+        Args: { "": unknown };
+        Returns: unknown;
+      };
+      gbt_int2_compress: {
+        Args: { "": unknown };
+        Returns: unknown;
+      };
+      gbt_int2_fetch: {
+        Args: { "": unknown };
+        Returns: unknown;
+      };
+      gbt_int4_compress: {
+        Args: { "": unknown };
+        Returns: unknown;
+      };
+      gbt_int4_fetch: {
+        Args: { "": unknown };
+        Returns: unknown;
+      };
+      gbt_int8_compress: {
+        Args: { "": unknown };
+        Returns: unknown;
+      };
+      gbt_int8_fetch: {
+        Args: { "": unknown };
+        Returns: unknown;
+      };
+      gbt_intv_compress: {
+        Args: { "": unknown };
+        Returns: unknown;
+      };
+      gbt_intv_decompress: {
+        Args: { "": unknown };
+        Returns: unknown;
+      };
+      gbt_intv_fetch: {
+        Args: { "": unknown };
+        Returns: unknown;
+      };
+      gbt_macad_compress: {
+        Args: { "": unknown };
+        Returns: unknown;
+      };
+      gbt_macad_fetch: {
+        Args: { "": unknown };
+        Returns: unknown;
+      };
+      gbt_macad8_compress: {
+        Args: { "": unknown };
+        Returns: unknown;
+      };
+      gbt_macad8_fetch: {
+        Args: { "": unknown };
+        Returns: unknown;
+      };
+      gbt_numeric_compress: {
+        Args: { "": unknown };
+        Returns: unknown;
+      };
+      gbt_oid_compress: {
+        Args: { "": unknown };
+        Returns: unknown;
+      };
+      gbt_oid_fetch: {
+        Args: { "": unknown };
+        Returns: unknown;
+      };
+      gbt_text_compress: {
+        Args: { "": unknown };
+        Returns: unknown;
+      };
+      gbt_time_compress: {
+        Args: { "": unknown };
+        Returns: unknown;
+      };
+      gbt_time_fetch: {
+        Args: { "": unknown };
+        Returns: unknown;
+      };
+      gbt_timetz_compress: {
+        Args: { "": unknown };
+        Returns: unknown;
+      };
+      gbt_ts_compress: {
+        Args: { "": unknown };
+        Returns: unknown;
+      };
+      gbt_ts_fetch: {
+        Args: { "": unknown };
+        Returns: unknown;
+      };
+      gbt_tstz_compress: {
+        Args: { "": unknown };
+        Returns: unknown;
+      };
+      gbt_uuid_compress: {
+        Args: { "": unknown };
+        Returns: unknown;
+      };
+      gbt_uuid_fetch: {
+        Args: { "": unknown };
+        Returns: unknown;
+      };
+      gbt_var_decompress: {
+        Args: { "": unknown };
+        Returns: unknown;
+      };
+      gbt_var_fetch: {
+        Args: { "": unknown };
+        Returns: unknown;
+      };
+      gbtreekey_var_in: {
+        Args: { "": unknown };
+        Returns: unknown;
+      };
+      gbtreekey_var_out: {
+        Args: { "": unknown };
+        Returns: unknown;
+      };
+      gbtreekey16_in: {
+        Args: { "": unknown };
+        Returns: unknown;
+      };
+      gbtreekey16_out: {
+        Args: { "": unknown };
+        Returns: unknown;
+      };
+      gbtreekey2_in: {
+        Args: { "": unknown };
+        Returns: unknown;
+      };
+      gbtreekey2_out: {
+        Args: { "": unknown };
+        Returns: unknown;
+      };
+      gbtreekey32_in: {
+        Args: { "": unknown };
+        Returns: unknown;
+      };
+      gbtreekey32_out: {
+        Args: { "": unknown };
+        Returns: unknown;
+      };
+      gbtreekey4_in: {
+        Args: { "": unknown };
+        Returns: unknown;
+      };
+      gbtreekey4_out: {
+        Args: { "": unknown };
+        Returns: unknown;
+      };
+      gbtreekey8_in: {
+        Args: { "": unknown };
+        Returns: unknown;
+      };
+      gbtreekey8_out: {
+        Args: { "": unknown };
+        Returns: unknown;
+      };
+    };
+    Enums: Record<never, never>;
+    CompositeTypes: Record<never, never>;
+  };
+}
+
+type DatabaseWithoutInternals = Omit<Database, "__InternalSupabase">;
+
+type DefaultSchema = DatabaseWithoutInternals[Extract<keyof Database, "public">];
+
+export type Tables<
+  DefaultSchemaTableNameOrOptions extends
+    | keyof (DefaultSchema["Tables"] & DefaultSchema["Views"])
+    | { schema: keyof DatabaseWithoutInternals },
+  TableName extends DefaultSchemaTableNameOrOptions extends {
+    schema: keyof DatabaseWithoutInternals;
+  }
+    ? keyof (DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"] &
+        DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Views"])
+    : never = never,
+> = DefaultSchemaTableNameOrOptions extends {
+  schema: keyof DatabaseWithoutInternals;
+}
+  ? (DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"] &
+      DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Views"])[TableName] extends {
+      Row: infer R;
+    }
+    ? R
+    : never
+  : DefaultSchemaTableNameOrOptions extends keyof (DefaultSchema["Tables"] & DefaultSchema["Views"])
+    ? (DefaultSchema["Tables"] & DefaultSchema["Views"])[DefaultSchemaTableNameOrOptions] extends {
+        Row: infer R;
+      }
+      ? R
+      : never
+    : never;
+
+export type TablesInsert<
+  DefaultSchemaTableNameOrOptions extends keyof DefaultSchema["Tables"] | { schema: keyof DatabaseWithoutInternals },
+  TableName extends DefaultSchemaTableNameOrOptions extends {
+    schema: keyof DatabaseWithoutInternals;
+  }
+    ? keyof DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"]
+    : never = never,
+> = DefaultSchemaTableNameOrOptions extends {
+  schema: keyof DatabaseWithoutInternals;
+}
+  ? DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+      Insert: infer I;
+    }
+    ? I
+    : never
+  : DefaultSchemaTableNameOrOptions extends keyof DefaultSchema["Tables"]
+    ? DefaultSchema["Tables"][DefaultSchemaTableNameOrOptions] extends {
+        Insert: infer I;
+      }
+      ? I
+      : never
+    : never;
+
+export type TablesUpdate<
+  DefaultSchemaTableNameOrOptions extends keyof DefaultSchema["Tables"] | { schema: keyof DatabaseWithoutInternals },
+  TableName extends DefaultSchemaTableNameOrOptions extends {
+    schema: keyof DatabaseWithoutInternals;
+  }
+    ? keyof DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"]
+    : never = never,
+> = DefaultSchemaTableNameOrOptions extends {
+  schema: keyof DatabaseWithoutInternals;
+}
+  ? DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+      Update: infer U;
+    }
+    ? U
+    : never
+  : DefaultSchemaTableNameOrOptions extends keyof DefaultSchema["Tables"]
+    ? DefaultSchema["Tables"][DefaultSchemaTableNameOrOptions] extends {
+        Update: infer U;
+      }
+      ? U
+      : never
+    : never;
+
+export type Enums<
+  DefaultSchemaEnumNameOrOptions extends keyof DefaultSchema["Enums"] | { schema: keyof DatabaseWithoutInternals },
+  EnumName extends DefaultSchemaEnumNameOrOptions extends {
+    schema: keyof DatabaseWithoutInternals;
+  }
+    ? keyof DatabaseWithoutInternals[DefaultSchemaEnumNameOrOptions["schema"]]["Enums"]
+    : never = never,
+> = DefaultSchemaEnumNameOrOptions extends {
+  schema: keyof DatabaseWithoutInternals;
+}
+  ? DatabaseWithoutInternals[DefaultSchemaEnumNameOrOptions["schema"]]["Enums"][EnumName]
+  : DefaultSchemaEnumNameOrOptions extends keyof DefaultSchema["Enums"]
+    ? DefaultSchema["Enums"][DefaultSchemaEnumNameOrOptions]
+    : never;
+
+export type CompositeTypes<
+  PublicCompositeTypeNameOrOptions extends
+    | keyof DefaultSchema["CompositeTypes"]
+    | { schema: keyof DatabaseWithoutInternals },
+  CompositeTypeName extends PublicCompositeTypeNameOrOptions extends {
+    schema: keyof DatabaseWithoutInternals;
+  }
+    ? keyof DatabaseWithoutInternals[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"]
+    : never = never,
+> = PublicCompositeTypeNameOrOptions extends {
+  schema: keyof DatabaseWithoutInternals;
+}
+  ? DatabaseWithoutInternals[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"][CompositeTypeName]
+  : PublicCompositeTypeNameOrOptions extends keyof DefaultSchema["CompositeTypes"]
+    ? DefaultSchema["CompositeTypes"][PublicCompositeTypeNameOrOptions]
+    : never;
+
+export const Constants = {
+  graphql_public: {
+    Enums: {},
+  },
+  public: {
+    Enums: {},
+  },
+} as const;
